00e063bd6d61da41faaba7f557d9dc1cd2b3c904
Andrew Chen <andrewxinchen@gmail.com>
2024-10-27 21:52:59 -0400

    resync2

--- generateCities.py
+++ generateCities.py
@@ -24,31 +24,31 @@ plat = ccrs.PlateCarree()
 fig, ax = plt.subplots(subplot_kw={"projection": crs}, figsize=(50, 40))
 
 ax.set_extent([-17, 40, 33, 70], crs=ccrs.PlateCarree())
-ax.add_feature(cfeature.NaturalEarthFeature(
-    category='cultural', name='admin_1_states_provinces',
-    scale=resolution, edgecolor='#535D8D', facecolor='none', zorder=10,
-    rasterized = True, antialiased = False, linewidth = 0.005))
-ax.add_feature(cfeature.NaturalEarthFeature(
-    category='physical', name='rivers_lake_centerlines',
-    scale=resolution, facecolor='none', edgecolor='#80ACCB', zorder=5,
-    rasterized = True, antialiased = False, linewidth = 0.005))
+# ax.add_feature(cfeature.NaturalEarthFeature(
+#     category='cultural', name='admin_1_states_provinces',
+#     scale=resolution, edgecolor='#535D8D', facecolor='none', zorder=10,
+#     rasterized = True, antialiased = False, linewidth = 0.005))
+# ax.add_feature(cfeature.NaturalEarthFeature(
+#     category='physical', name='rivers_lake_centerlines',
+#     scale=resolution, facecolor='none', edgecolor='#80ACCB', zorder=5,
+#     rasterized = True, antialiased = False, linewidth = 0.005))
 ax.add_feature(cfeature.NaturalEarthFeature(
     category='physical', name='lakes',
-    scale=resolution, facecolor='#D9F6FF', edgecolor='none', zorder=5,
+    scale=resolution, facecolor='#D9F6FF', edgecolor='#535D8D', zorder=5,
     rasterized = True, antialiased = False, linewidth = 0.005))
-ax.add_feature(cfeature.NaturalEarthFeature(
-    category='physical', name='ocean',
-    scale=resolution, facecolor='#D9F6FF', edgecolor='none', zorder=5,
-    rasterized = True, antialiased = False, linewidth = 0.005))
+# ax.add_feature(cfeature.NaturalEarthFeature(
+#     category='physical', name='ocean',
+#     scale=resolution, facecolor='#D9F6FF', edgecolor='none', zorder=5,
+#     rasterized = True, antialiased = False, linewidth = 0.005))
 
 
-cities = pd.read_csv('worldcities.csv', sep=',', lineterminator='\n')
-bigCities = cities[cities.population > 100000]
+# cities = pd.read_csv('worldcities.csv', sep=',', lineterminator='\n')
+# bigCities = cities[cities.population > 100000]
 
-points = ax.projection.transform_points(plat, bigCities.lng, bigCities.lat)
-ax.scatter(points[:, 0], points[:, 1], c='r', 
-    s=(72./fig.dpi)**2, zorder=5, marker=',',
-    rasterized=True, antialiased=False)
+# points = ax.projection.transform_points(plat, bigCities.lng, bigCities.lat)
+# ax.scatter(points[:, 0], points[:, 1], c='r', 
+#     s=(72./fig.dpi)**2, zorder=5, marker=',',
+#     rasterized=True, antialiased=False)
 
 plt.tight_layout()
 plt.savefig('geopEuroDivs2.png')
